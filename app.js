b85305db393ac62ad89ee383a1974ad7d4236cdd
farhanmaneri <farhanmaneri@gmail.com>
2023-06-15 13:46:33 +0500

    changes made

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 // IIFE Immediately invoked function Expressions
 (async function () {
-    const response = await fetch("./schools.json");
-    const schools = await response.json();
-     
-     const inputElem = document.getElementById("searchInput");
-    const btnElem = document.getElementById("searchBtn");
-    const listElem = document.getElementById("recipe-list");
-    const detailsElem = document.getElementById("recipeDetailsContainer");
-  
-    
-    function search() {
-      const query = inputElem.value.toLowerCase();
-      const results = schools.filter(function (recipe) {
-        return (recipe.TeacherName.toLowerCase().includes(query) ||
-        recipe.TeacherName.toLowerCase().includes(query))   
-      });
-      
-      displaySearchResults(results);
-      inputElem.value="";         
-    }
-    // search via enter button
-    inputElem.addEventListener('keyup', function(event){
-      if (event.key === 'Enter'){
-        event.preventDefault();
-        search();
-      }
-    })
+  const response = await fetch("./schools.json");
+  const schools = await response.json();
 
-    btnElem.addEventListener("click", search);  
-    function loadRecipeDetails(recipe) {
-      detailsElem.innerHTML = `
-          <h2 class="title">${recipe.TeacherName}</h2>
-          <h3>Ingredients:</h3>
-          <ul>${recipe.TeacherName.map(function (TeacherName) {
-            return "<li>" + TeacherName + "</li>"
-          }).join("")}</ul>
-          <h3>Instruction:</h3>
-          <div>${recipe.DoB}</div>
-      `;
+  const inputElem = document.getElementById("searchInput");
+  const btnElem = document.getElementById("searchBtn");
+  const listElem = document.getElementById("school-list");
+  const detailsElem = document.getElementById("schoolDetailsContainer");
+
+  // school search function
+  function search() {
+    const query = inputElem.value.toLowerCase();
+    const results = schools.filter(function (school) {
+      return (school.SchoolName.toLowerCase().includes(query)
+        // ||          school.Tehsil.toLowerCase().includes(query)
+      )
+    });
+
+    displaySearchResults(results);
+    inputElem.value = " ";
+  }
+  // search via enter button
+  inputElem.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      search();
     }
-   
-    function displaySearchResults (results) {
-      listElem.innerHTML = "";
-      results.forEach(function (recipe) {
-        const li = document.createElement("li");
-        const listItem = `
-            <h2 class="title">${recipe.TeacherName}</h2>
-            <div class="description">${recipe.DoB}</div>
+  })
+
+  // search via search button
+
+  btnElem.addEventListener("click", search);
+  function displaySearchResults(results) {
+    listElem.innerHTML = "";
+    results.forEach(function (school) {
+      const li = document.createElement("li");
+      const listItem = `
+            <h2 class="title">${school.SchoolName}</h2>
+            <div class="description"><h3>Emis Code ${school.EmisCode}</h3></div>
         `;
-        li.innerHTML = listItem;
-        li.addEventListener("click", function () {
-          loadRecipeDetails(recipe);
-        });
-        listElem.appendChild(li);
-      })
-    }
-  
-  })(); 
+      li.innerHTML = listItem;
+      // function for loading school detail on clicking li i.e school
+      li.addEventListener("click", function () {
+        loadschoolDetails(school);
+      });
+      listElem.appendChild(li);
+    })
+  }
+
+  function loadschoolDetails(school) {
+    detailsElem.innerHTML = `
+       
+           <h4 class="title">Emis Code: <u>  ${school.EmisCode}</u></h4>
+           <h4 class="title">Schoo Level: <u>  ${school.schoolLevel}</u></h4>
+           <h4 class="title">Total Enrollement: <u>  ${school.enrollement}</u></h4>
+           <h4 class="title">Working Teaching Staff:  <u> ${school.teachingStaff}</u></h4>
+           <h4 class="title">Working Non-teaching Staff: <u>  ${school.nonTeachingStaff}</u></p>
+           `;
+  }
+  // <h3>School Detail:</h3>
+  // return "<li>" + tehsil + "</li>"
+  // <h3>Instruction:</h3>
+  // <div>${school.tehsil}</div>
+  //  <ul>${school.tehsil.map(function (tehsil) {
+  // }).join("")}</ul>
+
+})(); 
